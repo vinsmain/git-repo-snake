c626b39821cbc6877f972ccab66e48083c483d1e
VINs <vinsmain@gmail.com>
2017-05-13 14:46:07 +0500

    Add move and controls

--- src/main/java/ru/mgusev/snake/FieldFrame.java
+++ src/main/java/ru/mgusev/snake/FieldFrame.java
@@ -1,8 +1,10 @@
 package ru.mgusev.snake;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -14,12 +16,29 @@ public class FieldFrame extends Application {
     private SnakeController snakeController;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(final Stage primaryStage) throws Exception {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Snake");
 
         initRootLayout();
+        snakeController.initField();
         snakeController.initSnakeHead();
+        snakeController.initSnakeTail();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (snakeController.getColumnIndex() > 0 && snakeController.getColumnIndex() < 20 && snakeController.getRowIndex() > 0 && snakeController.getRowIndex() < 20 ){
+                        //snakeController.getFieldGP().getChildren().clear();
+                        snakeController.move();
+                        Thread.sleep(500);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     public void initRootLayout() {
@@ -35,6 +54,29 @@ public class FieldFrame extends Application {
             snakeController = loader.getController();
             snakeController.setMainApp(this);
             primaryStage.show();
+
+            scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+                public void handle(KeyEvent key) {
+                    String pressedKey = key.getCode().getName();
+                    //try {
+                        if (pressedKey.equals("Up")) {
+                            snakeController.setAngle(0.0);
+                            //snakeController.move();
+                        } else if (pressedKey.equals("Down")) {
+                            snakeController.setAngle(180.0);
+                            //snakeController.move();
+                        } else if (pressedKey.equals("Right")) {
+                            snakeController.setAngle(90.0);
+                            //snakeController.move();
+                        } else if (pressedKey.equals("Left")) {
+                            snakeController.setAngle(270.0);
+                            //snakeController.move();
+                        }
+                    /*} catch (//InterruptedException e) {
+                        //e.printStackTrace();
+                    }*/
+                }
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/ru/mgusev/snake/SnakeController.java
+++ src/main/java/ru/mgusev/snake/SnakeController.java
@@ -1,28 +1,123 @@
 package ru.mgusev.snake;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 public class SnakeController {
 
     private FieldFrame mainApp;
+    private ImageView imageView;
+    private Double angle = 0.0;
     private int columnIndex = 5;
     private int rowIndex = 5;
+    private int columnIndexTail = 5;
+    private int rowIndexTail = 4;
 
     @FXML
-    private GridPane fieldGP = new GridPane();
+    private GridPane fieldGP  = new GridPane();
 
     @FXML
-    private Button snakeHead = new Button();
+    private Button snakeHead;
+
+    @FXML
+    private Button snakeTail;
 
     public void initSnakeHead() {
-        snakeHead.setPrefWidth(50);
-        snakeHead.setPrefHeight(50);
-        fieldGP.add(snakeHead, columnIndex, rowIndex);
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                snakeHead = new Button();
+                Image imageDecline = new Image(getClass().getResourceAsStream("/images/head.png"));
+                imageView = new ImageView(imageDecline);
+                imageView.setRotate(angle);
+                snakeHead.setGraphic(imageView);
+                snakeHead.setPrefWidth(50);
+                snakeHead.setPrefHeight(50);
+                snakeHead.setPadding(Insets.EMPTY);
+                fieldGP.add(snakeHead, columnIndex, rowIndex);
+                GridPane.setHalignment(snakeHead, HPos.CENTER);
+                GridPane.setValignment(snakeHead, VPos.CENTER);
+            }
+        });
+    }
+
+    public void initSnakeTail() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                snakeTail = new Button();
+                Image imageDecline = new Image(getClass().getResourceAsStream("/images/body.png"));
+                ImageView imageView = new ImageView(imageDecline);
+                snakeTail.setGraphic(imageView);
+                snakeTail.setPrefWidth(50);
+                snakeTail.setPrefHeight(50);
+                snakeTail.setPadding(Insets.EMPTY);
+                fieldGP.add(snakeTail, columnIndexTail, rowIndexTail);
+                GridPane.setHalignment(snakeTail, HPos.CENTER);
+                GridPane.setValignment(snakeTail, VPos.CENTER);
+            }
+        });
+    }
+
+    public void initField() {
+        for (int i = 0; i < 10; i++) {
+            RowConstraints row = new RowConstraints(54);
+            ColumnConstraints column = new ColumnConstraints(54);
+            fieldGP.getRowConstraints().add(row);
+            fieldGP.getColumnConstraints().add(column);
+        }
+        fieldGP.setGridLinesVisible(true);
+        fieldGP.setStyle("-fx-background-color: greenyellow; -fx-grid-lines-visible: true");
     }
 
     public void setMainApp(FieldFrame mainApp) {
         this.mainApp = mainApp;
+    }
+
+    public void setAngle(Double angle) {
+        this.angle = angle;
+    }
+
+    public void move() throws InterruptedException {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                fieldGP.getChildren().clear();
+                rowIndexTail = rowIndex;
+                columnIndexTail = columnIndex;
+                if (angle == 0.0) {
+                    rowIndex--;
+                } else if (angle == 180) {
+                    rowIndex++;
+                } else if (angle == 90) {
+                    columnIndex++;
+                } else if (angle == 270) {
+                    columnIndex--;
+                }
+                initSnakeHead();
+                initSnakeTail();
+            }
+        });
+    }
+
+    public int getColumnIndex() {
+        return columnIndex;
+    }
+
+    public int getRowIndex() {
+        return rowIndex;
+    }
+
+    public GridPane getFieldGP() {
+        return fieldGP;
     }
 }
